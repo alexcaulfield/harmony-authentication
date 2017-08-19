8fd80cad39a82e20b688a9ac2d94cf87cd632af8
Alex Caulfield <alex.caulfield28@gmail.com>
2017-08-19 18:10:30 -0400

    added logic to create playlist and add related songs to playlist

--- auth.js
+++ auth.js
@@ -26,26 +26,31 @@ var scopes = ['playlist-modify-public', 'user-top-read'],
 var spotifyApi = new SpotifyWebApi({
   redirectUri: redirectUri,
   clientId: clientId,
-  clientSecret : '3f04f5630aa04a889157d3cbccc8b1d3'
+  clientSecret : 'f6873873a0254442af4d6b5f54d57880'
 });
+
+// When our access token will expire
+var tokenExpirationEpoch;
 
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
 app.get('/', function(request, response) {
+  console.log('in get /')
+  console.log(authorizeURL)
 
   // var access_token = '';
-  
-  // Retrieve an access token via authorization code workflow 
+
+  // Retrieve an access token via authorization code workflow
   response.send(authorizeURL);
-  
+
 });
 
 app.get('/callback', function(request, response){
   var code = request.query.code;
   var state = request.query.state;
-  
+
   if (state === 'SPACEBROWNS'){
-    
+
     spotifyApi.authorizationCodeGrant(code).then(function(data){
 
       console.log('The token expires in ' + data.body['expires_in']);
@@ -55,13 +60,17 @@ app.get('/callback', function(request, response){
       spotifyApi.setAccessToken(data.body['access_token']);
       spotifyApi.setRefreshToken(data.body['refresh_token']);
 
+    // Save the amount of seconds until the access token expired
+    tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+    console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+
       response.redirect("/finished");
 
     }, function(err){
       response.send(err);
       console.log("Something went wrong", err);
     });
-  }else {
+  } else {
     response.send("I didn't send you here... please leave");
   }
 })
@@ -71,17 +80,89 @@ app.get('/finished', function(request, response) {
   spotifyApi.getMe().then(function(data){
     response.send(data.body);
   });
-  
+
 });
 
+var artistId;
+var relatedArtistsIds = [];
+var playlistTrackUris = [];
+var playlistId;
 
-spotifyApi.refreshAccessToken().then(function(data){
-  console.log('The access token is being refreshed');
-
-  spotifyApi.setAccessToken(data.body['access_token']);
-}, function(err){
-  console.log('Could not refresh token', err);
+app.get('/findArtist', function(request, response) {
+  spotifyApi.searchArtists('The Chainsmokers')
+  .then(function(data) {
+    artistId = data.body.artists.items[0].id;
+    spotifyApi.createPlaylist('cauf', 'Test Playlist', { 'public' : true })
+      .then(function(data) {
+        console.log('Created playlist!');
+        playlistId = data.body.id;
+        spotifyApi.getArtistRelatedArtists(artistId)
+          .then(function(data) {
+            for (var i = 0; i < 5; i++) {
+              relatedArtistsIds.push(data.body.artists[i].id);
+            }
+            for (var k = 0; k < 5; k++) {
+              spotifyApi.getArtistTopTracks(relatedArtistsIds[k], 'US')
+                .then(function(data) {
+                  for (var j = 0; j < 5; j++) {
+                    // console.log(data.body.tracks[j]);
+                    playlistTrackUris.push(data.body.tracks[j].uri);
+                  }
+                  // response.send(data.body.tracks);
+                }, function(err) {
+                  console.log(err);
+                });
+            }
+            // console.log('out of for loop and in RelatedArtists');
+            // console.log(playlistTrackUris);
+            spotifyApi.addTracksToPlaylist('cauf', playlistId, playlistTrackUris)
+              .then(function(data) {
+                console.log('Added tracks to playlist!');
+              }, function(err) {
+                console.log('Something went wrong!', err);
+              });
+          }, function(err) {
+            console.log(err);
+          });
+      }, function(err) {
+        console.log('Something went wrong!', err);
+      });
+  }, function(err) {
+    console.error(err);
+  });
 });
+
+var topTracks = function(artist) {
+  spotifyApi.getArtistTopTracks(artist, 'US')
+    .then(function(data) {
+      for (var j = 0; j < 5; j++) {
+        tracks.push(data.body.tracks[j].id);
+      }
+      // response.send(data.body);
+    }, function(err) {
+      console.log(err);
+    });
+}
+
+var numberOfTimesUpdated = 0;
+
+setInterval(function() {
+  console.log('Time left: ' + Math.floor((tokenExpirationEpoch - new Date().getTime() / 1000)) + ' seconds left!');
+
+  // OK, we need to refresh the token. Stop printing and refresh.
+  if (++numberOfTimesUpdated > 5) {
+    clearInterval(this);
+
+    // Refresh token and print the new time to expiration.
+    spotifyApi.refreshAccessToken()
+      .then(function(data) {
+        tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+        console.log('Refreshed token. It now expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+      }, function(err) {
+        console.log('Could not refresh the token!', err.message);
+      });
+  }
+}, 1000);
 
 
 app.listen(app.get('port'), function() {
